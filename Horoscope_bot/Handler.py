91d37e335fe7ba94d37e0a517eeb44f92fec407b
Riccardo Cuccia <riccardo.cuccia02@gmail.com>
2022-09-23 10:10:52 +0200

    feat: simple API request

--- Horoscope_bot/Handler.py
+++ Horoscope_bot/Handler.py
@@ -3,6 +3,7 @@ from telegram.ext import CommandHandler
 from telegram import Update
 from telegram.ext import CallbackContext
 from telegram.ext import Updater
+import requests
 
 
 updater = Updater(token='TOKEN_PLACEHOLDER')
@@ -16,7 +17,15 @@ start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
 def echo(update: Update, context: CallbackContext):
-    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+    params = (
+        ('sign', update.message.text),
+        ('day', 'today')
+    )
+    response = requests.post('https://aztro.sameerkumar.website/', params=params)
+    jsonResponse = response.json()
+
+
+    context.bot.send_message(chat_id=update.effective_chat.id, text = jsonResponse["description"])
 
 echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
 dispatcher.add_handler(echo_handler)
