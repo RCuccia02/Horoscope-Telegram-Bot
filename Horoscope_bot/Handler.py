1ed3667fd31a008a8e8b8db8ba6fdcc58326686d
Riccardo Cuccia <riccardo.cuccia02@gmail.com>
2022-10-05 11:49:38 +0200

    refactor: renamed echo to kb_message

--- Horoscope_bot/Handler.py
+++ Horoscope_bot/Handler.py
@@ -25,5 +25,5 @@ dispatcher.add_handler(start_handler)
 def echo(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
-echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-dispatcher.add_handler(echo_handler)
+kb_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+dispatcher.add_handler(kb_handler)
