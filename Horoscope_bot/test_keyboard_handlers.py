c091c5ce3df706587fbfe89def98f7bbfe9a9363
sergioyc40 <sergioyc40@gmail.com>
2022-10-06 17:01:26 +0200

    test: added test for echo function

---
+++ Horoscope_bot/test_keyboard_handlers.py
@@ -0,0 +1,30 @@
+from pytest_mock import MockerFixture
+
+
+from Handler import start, echo
+
+
+class TestHandlers:
+
+#context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+    def test_echo(self, mocker: MockerFixture):
+        context = mocker.MagicMock()
+        update = mocker.MagicMock()
+        bot = mocker.MagicMock()
+        effective_chat = mocker.MagicMock()
+        message = mocker.MagicMock()
+        send_message = mocker.MagicMock()
+
+        bot.send_message = send_message
+        effective_chat.id = 10
+        message.text = "text"
+
+        context.bot = bot
+        update.effective_chat = effective_chat
+        update.message = message
+
+        echo(update, context)
+
+        send_message.assert_called_once_with(chat_id=10, text="text")
+
