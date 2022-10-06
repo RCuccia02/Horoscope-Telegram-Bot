1a97832d8da5669948a759f8940a6d5cecb7186e
sergioyc40 <sergioyc40@gmail.com>
2022-10-06 17:10:36 +0200

    test: added test for start function

--- Horoscope_bot/test_keyboard_handlers.py
+++ Horoscope_bot/test_keyboard_handlers.py
@@ -28,3 +28,23 @@ class TestHandlers:
 
         send_message.assert_called_once_with(chat_id=10, text="text")
 
+    def test_start(self, mocker: MockerFixture):
+        context = mocker.MagicMock()
+        update = mocker.MagicMock()
+        bot = mocker.MagicMock()
+        effective_chat = mocker.MagicMock()
+        message = mocker.MagicMock()
+        send_message = mocker.MagicMock()
+
+        bot.send_message = send_message
+        effective_chat.id = 10
+        message.text = "I'm a bot, please talk to me!"
+
+        context.bot = bot
+        update.effective_chat = effective_chat
+        update.message = message
+
+        start(update, context)
+
+        send_message.assert_called_once_with(chat_id=10, text="I'm a bot, please talk to me!")
+
